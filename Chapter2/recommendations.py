af29d56cc2a9e59f6def94a544665ab5939fd539
Borwe <msbhbc@MSBHBCT006.MSBHK.TPSKENYA.co.ke>
2018-05-28 17:35:38 +0300

    Job Update

--- Chapter2/recommendations.py
+++ Chapter2/recommendations.py
@@ -1,5 +1,7 @@
 # A dictionary of movie critics and their ratings of a small
 # set of movies
+from math import sqrt
+
 
 critics={'Lisa Rose':{'Lady in the Water':2.5,'Snakes on a Plane':3.5,
                       'Just My Luck':3.0,'Superman Returns':3.5,
@@ -21,3 +23,21 @@ critics={'Lisa Rose':{'Lady in the Water':2.5,'Snakes on a Plane':3.5,
          'Toby':{'Snakes on a Plane':4.0,'You Me and Dupree':1.0,
                          'Superman Returns':4.0}
          }
+
+#returns a distance-based similarity score for person1 and person2
+def sim_distance(prefs,person1,person2):
+    #Get the list of shared_items
+    si={}
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item]=1
+
+    #if they have no ratings in common reurn 0
+    if len(si)==0:
+        return 0
+
+    #Add up the squares of all the differences
+    sum_of_squares=([pow(prefs[person1][item]-prefs[person2][item],2)
+                     for item in prefs[person1] if item in prefs[person2]])
+
+    return 1/(1+sum_of_squares)
